ccba3677a559b2d17ef71f63855306790f4079b4
FlorianBorie <florian.borie@ynov.com>
2021-12-10 15:31:38 +0100

    connexion avec la bdd

--- Back/server.js
+++ Back/server.js
@@ -12,11 +12,29 @@ routes.use(bodyParser.json());
  
 //cors
 routes.use(cors());
- 
+
+//connexion au client
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://toto:Azerty@cluster-clients.naa1h.mongodb.net/clients?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//connexion à la base de donnée
+client.connect(err => {
+    if(err) {
+        throw Error(err);
+    }
+  const collection = client.db("clients").collection("personnes");
+  console.log(`Success et connexion avec la basse de donnée`);
+  // perform actions on the collection object
+  client.close();
+});
+
+
+//port
 app.listen(PORT, () => {
   console.log(`Mon serveur écoute sur http://localhost:${PORT}`);
 });
- 
+
+//routes
 routes.get("/", (req, res) => {
   res.send("Hello World!");
 });
