ef9644ffb007bc9d46accd3d876b9070402f39d8
FlorianBorie <florian.borie@ynov.com>
2022-01-07 11:36:31 +0100

    begin the navigation between page

---
+++ Front/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+    <div className="shopping-list">
+        <h1>Bienvenue sur la page HOME !</h1>
+    </div>
+    );
+}
+
+export default Home;
