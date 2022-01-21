0cb71d18d0d51a7dbc6ff0410e45d7c5433bf633
florian <florian.lys@ynov.com>
2022-01-21 16:02:54 +0100

    mail

---
+++ Front/src/components/Auth/Login/Contact.js
@@ -0,0 +1,25 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export default function Contact() {
+  const form = useRef();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6u6we2i', 'template_scsq9je', form.current, 'user_L2OhZMvSPR4uKFbbo4TGD')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
--- Front/src/components/Layout/Navbar.js
+++ Front/src/components/Layout/Navbar.js
@@ -3,12 +3,14 @@ import { Navbar, Nav, Container} from 'react-bootstrap';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import emailjs from '@emailjs/browser';
 
 // Pages
 import Home from '../Home/Home';
 import AddClient from "../Cart/AddClient";
 import Login from "../Auth/Login/Login";
 import Register from "../Auth/Register/Register";
+import  Contact  from "../Auth/Login/Contact";
 
 function NavBar() {
     return (
@@ -31,7 +33,7 @@ function NavBar() {
                     <Route exact path='/login' element={<Login/>}/>
                     <Route exact path='/' element={<Home/>}/>
                     <Route exact path='/add' element={<AddClient/>}/>
-                    <Route exact path='/forbidden' element={forbidden()}/>
+                    <Route exact path='/forbidden' element={<Contact/>}/>
                     {/* <Route exact path='/register' element={<Register/>}/>
                     <Route exact path='/login' element={<Login/>}/> */}
                 </Routes>
@@ -39,21 +41,37 @@ function NavBar() {
         </BrowserRouter>
     );
 }
-function forbidden(){
-    return(
-        <div>
-            <h1>Vous avez oublié votre mot de passe !</h1>
-            <Form className='FormLogin'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Mettez votre mail</Form.Label>
-                    <Form.Control type="email" placeholder="Entrer votre email" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                        Valider
-                </Button>
-            </Form>
+// function forbidden(){
 
-        </div>
-    )
-}
+//     export const Contact = () => {
+//         const form = useRef();
+      
+//         const sendEmail = (e) => {
+//           e.preventDefault();
+      
+//           emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+//             .then((result) => {
+//                 console.log(result.text);
+//             }, (error) => {
+//                 console.log(error.text);
+//             });
+//         };
+//     }
+
+//     return(
+//         <div>
+//             <h1>Vous avez oublié votre mot de passe !</h1>
+//             <Form className='FormLogin' onSubmit={sendEmail}>
+//                 <Form.Group className="mb-3" controlId="formBasicEmail">
+//                     <Form.Label>Mettez votre mail</Form.Label>
+//                     <Form.Control type="email" placeholder="Entrer votre email" />
+//                 </Form.Group>
+//                 <Button variant="primary" type="submit">
+//                         Valider
+//                 </Button>
+//             </Form>
+
+//         </div>
+//     )
+// }
 export default NavBar;
