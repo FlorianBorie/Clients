d901b876bda29228a31fdfbdf0d45b499994db04
FlorianBorie <florian.borie@ynov.com>
2022-01-21 15:32:51 +0100

    begin register

--- Front/src/components/Auth/Login/Login.js
+++ Front/src/components/Auth/Login/Login.js
@@ -1,36 +1,64 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import './Login.css'
+import React, {useEffect} from 'react';
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import {useFormValidation} from '../../../lib/hooks/useFormValidation/index';
+import * as Input from '../Input';
+import './Login.css';
 
-function Login() {
-    return (
-    <div className="shopping-list">
-        <h1>Connectez-vous !</h1>
-        <div className='FormLogin'>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Entrer votre email" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Mot de passe</Form.Label>
-                    <Form.Control type="password" placeholder="Entrer votre mot de passe" />
-                    {/* <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text> */}
-                </Form.Group>
-                <div>
-                    <a href='/forbidden'>Mot de passe oublié !</a>
-                    <Button variant="primary" type="submit">
-                        Valider
-                    </Button>
-                </div>
-            </Form>
-        </div>
+const Alert = ({ isVisible }) => (
+	isVisible &&
+	<div className="alert alert-info mt-3">
+		<p className="icontext"><i className="icon text-primary fa fa-thumbs-up"></i>User successfully connected</p>
     </div>
-    );
+)
+const ErrorMessage = ({ error }) => (
+	error && 
+	<div className="alert alert-danger mt-3">
+		<p className="icontext]" style={{ color: 'crimson' }}><i className="icon text-danger fas fa-exclamation-circle"></i> {' '}{error?.error}</p>
+    </div>
+)
+
+const defaultValues = {
+	email: 'sandy@gmail.com' ,
+	password: '' ,
 }
 
-export default Login;
+const Login = () => { 
+    const navigation = useNavigate();
+    const {formValues, validate, register, handleOnChange, isValid} = useFormValidation({formName: "login"});
+    const {email, password} = formValues["login"] ?? {};
+    // useEffect(() => {
+    //     register(defaultValues);
+    // }, []);
+
+    useEffect(() => {
+        validate(formValues["login"] ?? {})
+    }, [formValues]);
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        setTimeout(Navigate.push('/'), 2000)
+    };
+
+  return(<>
+		<div className="card mx-auto" style={{maxWidth: '380px', marginTop:'200px'}}>
+        <div className="card-body">
+            <h4 className="card-title mb-4">Se connecter</h4>
+            <form name="login" onSubmit={handleOnSubmit}>
+                <div className="form-group" style={{ marginBottom: 0 }}>
+                    <Input.Email label="Email" style={{padding: 0}} onChange={handleOnChange}/>
+                </div>
+                <div className="form-group">
+                    <Input.Password label="Mot de passe" name="password" style={{padding: 0}}  onChange={handleOnChange} />
+                </div>
+                <div className="form-group">
+                    <Input.Submit classNames="btn-primary btn-block" title="Login" disabled={!isValid}/> 
+			    </div> 
+            </form>
+        </div>
+        </div> 
+        <p className="text-center mt-4">Vous pouvez vous inscrire ici ! <Link to='/register'>S'inscrire</Link></p>
+        <p className="text-center mt-4">Vous avez un trou de mémoire cliqué ici <Link to='/forbidden'>Mot de passe oublié !</Link></p>
+		<br /><br />
+	</>)
+}  
+export default Login
--- Front/src/components/Auth/Register/Register.js
+++ Front/src/components/Auth/Register/Register.js
@@ -1,43 +1,120 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import './Register.css'
+import React, {useEffect} from 'react';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+import { Provider, useDispatch } from 'react-redux'
+import {Link, useNavigate} from 'react-router-dom';
+import { useFormValidation } from '../../../lib/hooks/useFormValidation/index';
+import * as Input from "../Input";
+import './Register.css';
+import useAuthentication from '../../../lib/hooks/useAuthentication/index';
 
-function Register() {
-    return (
-    <div className="shopping-list">
-        <h1>Inscrivez-vous !</h1>
-        <div className='FormRegister'>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicPrenom">
-                    <Form.Label>Prénom</Form.Label>
-                    <Form.Control type="text" placeholder="Entrer votre prenom" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicNom">
-                    <Form.Label>Nom</Form.Label>
-                    <Form.Control type="text" placeholder="Entrer votre nom" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Entrer votre email" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Mot de passe</Form.Label>
-                    <Form.Control type="password" placeholder="Entrer votre mot de passe" />
-                    {/* <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text> */}
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    S'inscrire
-                </Button>
-            </Form>
-        </div>
+const Alert = ({ isVisile }) => {
+    isVisile &&
+    <div className='alert alert-info mt-3'>
+        <p className='icontext'><i className='icon text-primary fa fa-thumbs-up'></i>L'utilisateur à été ajouté avec success</p>
     </div>
+}
+
+const ErrorMessage = ({ error }) => {
+    error &&
+    <div className='alert alert-danger mt-3'>
+        <p className='icontext' style={{color: "crimson"}}><i className='icon text-danger fas fa-exclamation-circle'></i>{' '}{error?.error}</p>
+    </div>
+}
+
+// const defaultValues = {
+//     prenom: "sandy",
+//     nom: "last",
+//     mail: "sandy@gmail.com",
+//     password: "test12345",
+//   };
+
+const Register = ({history}) => {
+  // const dispatch = useDispatch();
+  const { handleUserRegistration } = useAuthentication();
+  // const navigation = useNavigate();
+  const {formValues, validate, register, handleOnChange, isValid} = useFormValidation({formName: "register"});
+  const {prenom, nom, email, password} = formValues["register"] ?? {};
+  // useEffect(() => {
+  //     register(defaultValues);
+  // }, []);
+
+  useEffect(() => {
+      validate(formValues["register"] ?? {})
+  }, [formValues]);
+
+  const handleOnSubmit = (e) => {
+      e.preventDefault();
+      const newUser = {
+        prenom,
+        nom,
+        email,
+        password
+      };
+      handleUserRegistration(newUser).then(() => {
+        console.log("Utilisateur bien enregistré !")
+        // setTimeout(() => history.push("/", 2000));
+      })
+      // navigation.push("/");
+  };
+
+    return (
+        <>
+        <div
+          className="card mx-auto"
+          style={{ maxWidth: "520px", marginTop: "140px" }}
+        >
+          <article className="card-body">
+            <header className="mb-4">
+              <h4 className="card-title">S'inscrire</h4>
+            </header>
+            {/* feedback et message d'erreurs */}
+            <form name="register" onSubmit={handleOnSubmit}>
+              <div className="form-row">
+                <Input.Text
+                  label="Prénom"
+                  name="prenom"
+                  onChange={handleOnChange}
+                />
+                <Input.Text
+                  label="Nom"
+                  name="nom"
+                  onChange={handleOnChange}
+                />
+              </div>
+              <div className="form-group">
+                <Input.Email
+                  label="Mail"
+                  style={{ padding: 0 }}
+                  onChange={handleOnChange}
+                />
+              </div>  
+              <div className="form-row">
+                <Input.Password
+                  label="Rentrer votre mot de passe"
+                  onChange={handleOnChange}
+                />
+              </div>
+              <div className="form-group">
+                <a href='"/'>
+                  <Input.Submit
+                    classNames="btn-primary btn-block"
+                    title="S'inscrire"
+                    disabled={!isValid}
+                  />
+
+                </a>
+              </div>
+            </form>
+          </article>
+        </div>
+        <p className="text-center mt-4">
+          As-tu déjà un compte ? <Link to="/login">Se connecter</Link>
+        </p>
+        <br />
+        <br />
+        <br />
+      </>
     );
 }
 
